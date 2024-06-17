a827005fda75ea38c3cf6321efae226c92282969
Ryan <ryanfox1@hotmail.co.uk>
2024-06-17 19:02:57 +0100

    2nd commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,96 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import Link from './components/Link';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Link from "./components/Link";
 
 function App() {
   const [url, setUrl] = useState("");
-  const [urlList, setUrlList] = useState<urlStates>([])
+  const [urlList, setUrlList] = useState<UrlStates>([]);
 
-  type urlStates = [string, string][];
-  
+  type UrlStates = {
+    slug: string;
+    url: string;
+  }[];
 
   const createUrl = async () => {
-    const response = await fetch('http://localhost:4000', {
+    const response = await fetch("http://localhost:4000", {
       method: "POST",
       body: JSON.stringify({ url }),
       headers: {
         "Content-Type": "application/json",
       },
-    })
+    });
 
-    getLinks()
-  }
+    getLinks();
+  };
 
   useEffect(() => {
-    getLinks()
+    getLinks();
   }, []);
 
   async function getLinks() {
     const response = await fetch("http://localhost:4000/links");
 
-    const data = await response.json()
-    setUrlList(Object.entries(data))
+    const data = await response.json();
+    setUrlList(data);
   }
 
   async function remove(link: string) {
     await fetch(`http://localhost:4000/links/${link}`, {
       method: "DELETE",
     });
-    console.log(link)
+    console.log(link);
     getLinks();
   }
 
   return (
     <div>
-      <nav className='w-1/3 mx-auto'> 
-        <ul className='flex gap-6  font-light justify-center text-white mt-10'>
+      <nav className="w-1/3 mx-auto">
+        <ul className="flex gap-6  font-light justify-center text-white mt-10">
           <li>Home</li>
           <li>QR Code Generator</li>
           <li>Stats</li>
         </ul>
       </nav>
-      <h1 className='w-auto mx-auto text-center text-white font-extrabold mt-20 text-4xl tracking-wider'>Ryans URL shortener</h1>
-      <p className='flex pt-4 text-gray-500 font-light w-2/3 justify-center content-center mx-auto'>RB.GY is a free tool to shorten URLs powered by Rebrandly. Create short & memorable links in seconds.</p>
-      <div className='flex h-auto flex-col w-2/3 mx-auto rounded-lg items-center mt-8'>
-        <div className='flex flex-col justify-evenly'>
-          <div className='items-center gap-3 mb-10 flex'>
-            <input className='w-96 pl-3 my-auto rounded-md h-12 text-lg ' placeholder="Enter Link Here"type='text' name='url' value={url} onChange={(e) => { setUrl(e.target.value) }}></input>
-            <button className='border-solid my-auto rounded-md border-blue-400 text-white bg-blue-400 h-12 border-2 p-2' onClick={createUrl}>Shorten URL</button>
+      <h1 className="w-auto mx-auto text-center text-white font-extrabold mt-20 text-4xl tracking-wider">
+        Ryans URL shortener
+      </h1>
+      <p className="flex pt-4 text-gray-500 font-light w-2/3 justify-center content-center mx-auto">
+        RB.GY is a free tool to shorten URLs powered by Rebrandly. Create short
+        & memorable links in seconds.
+      </p>
+      <div className="flex h-auto flex-col w-2/3 mx-auto rounded-lg items-center mt-8">
+        <div className="flex flex-col justify-evenly">
+          <div className="items-center gap-3 mb-10 flex">
+            <input
+              className="w-96 pl-3 my-auto rounded-md h-12 text-lg "
+              placeholder="Enter Link Here"
+              type="text"
+              name="url"
+              value={url}
+              onChange={(e) => {
+                setUrl(e.target.value);
+              }}
+            ></input>
+            <button
+              className="border-solid my-auto rounded-md border-blue-400 text-white bg-blue-400 h-12 border-2 p-2"
+              onClick={createUrl}
+            >
+              Shorten URL
+            </button>
           </div>
-          <div className='flex gap-3 flex-col'>
-          {urlList.map((url) => <Link 
-          slug={url[1]} 
-          originalLink={url[0]}
-          onDelete={() => remove(url[0])} />)}
+          <div className="flex gap-3 flex-col">
+            {urlList.map((url, index) => (
+              <Link
+                key={index}
+                slug={url.slug}
+                originalLink={url.url}
+                onDelete={() => remove(url.slug)}
+              />
+            ))}
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
-export default App
+export default App;
--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,13 +1,25 @@
-export default function Link(props) {
-    return (
-        <div className=" gap-3 flex flex-row h-12">
-            <div className="bg-white flex flex-row justify-between rounded-lg w-96 px-3 pt-3">
-                <p>http://localhost:5173/{props.slug}</p>
-                <p className="text-gray-500">{props.originalLink}</p>
-            </div>
-            <button onClick={props.onDelete} className='border-solid my-auto rounded-md border-blue-400 text-white bg-blue-400 h-12 border-2 p-2'>Delete Link</button>
-            <button className='border-solid my-auto rounded-md border-blue-400 text-white bg-blue-400 h-12 border-2 p-2'>Copy Link</button>
+type LinkProps = {
+  slug: string;
+  originalLink: string;
+  onDelete: () => void;
+};
 
-        </div>
-    )
+export default function Link(props: LinkProps) {
+  return (
+    <div className=" gap-3 flex flex-row h-12">
+      <div className="bg-white flex flex-row justify-between rounded-lg w-96 px-3 pt-3">
+        <p>http://localhost:5173/{props.slug}</p>
+        <p className="text-gray-500">{props.originalLink}</p>
+      </div>
+      <button
+        onClick={props.onDelete}
+        className="border-solid my-auto rounded-md border-blue-400 text-white bg-blue-400 h-12 border-2 p-2"
+      >
+        Delete Link
+      </button>
+      <button className="border-solid my-auto rounded-md border-blue-400 text-white bg-blue-400 h-12 border-2 p-2">
+        Copy Link
+      </button>
+    </div>
+  );
 }
